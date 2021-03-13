dbee40673ecf6926f73104427c1b5ac06099bbaa
陈磊 <gbdvjxu1257q@outlook.com>
2021-03-13 14:52:39 +0800

    临时提交

--- src/Snow.Hcm.Web/abp.resourcemapping.js
+++ src/Snow.Hcm.Web/abp.resourcemapping.js
@@ -1,8 +1,9 @@
 ﻿module.exports = {
     aliases: {
-        
+        "@SourceLib": "./lib"
     },
     mappings: {
-        
+        "@node_modules/daterangepicker/daterangepicker.js": "@libs/daterangepicker/",
+        "@node_modules/daterangepicker/daterangepicker.css": "@libs/daterangepicker/",
     }
 };
